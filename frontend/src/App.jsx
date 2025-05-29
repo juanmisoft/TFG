0ea37774d5e2ce34ac153dcdb896a415d17887c7
juanmisoft <132227030+juanmisoft@users.noreply.github.com>
2025-05-30 01:03:08 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,8 @@ import Login from './pages/Login';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import Promotions from './pages/Promotions';
-import Requests from './pages/Requests'; // Asumo que existe
-import Calendar from './pages/Calendar'; // Asumo que existe
+import Requests from './pages/Requests'; 
+import Calendar from './pages/Calendar'; 
 import News from './pages/News';
 import ArchivedNews from './pages/ArchivedNews';
 import UserManagement from './pages/UserManagement';
