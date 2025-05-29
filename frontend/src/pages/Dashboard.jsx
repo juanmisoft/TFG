94843543ccbc2245bcb672c01a609ab5a2c20542
juanmisoft <132227030+juanmisoft@users.noreply.github.com>
2025-05-30 01:06:09 +0200

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import jsPDF from 'jspdf';
-import autoTable from 'jspdf-autotable'; // Cambiamos el import
+import autoTable from 'jspdf-autotable';
 
 function Dashboard() {
   const [currentUser, setCurrentUser] = useState(null);
